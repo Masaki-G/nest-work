1374445447c9e68a13f38e77530f0d91c2500cf6
Masaki Go <masakigo@MasakinoMacBook-Pro.local>
2023-03-11 16:24:39 +0900

    created TasksController

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,46 @@
+import { TasksService } from './task.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Delete,
+} from '@nestjs/common';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './task.entity';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.create(createTaskDto);
+  }
+
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.tasksService.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.tasksService.delete(id);
+  }
+}
